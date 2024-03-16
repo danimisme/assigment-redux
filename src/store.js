0f4d9808801381fa5098eb41b0e3aab14e3c0420
danimisme <msubhanr53@gmail.com>
2024-03-16 22:37:28 +0700

    fix import composeWithDevTools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import { createStore, combineReducers } from "redux";
 import cartReducer from "./features/cart/cartSlice";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
