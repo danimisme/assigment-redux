7d55bdd66cb86b3d1093fe4887a708aae04d4982
danimisme <msubhanr53@gmail.com>
2024-03-16 22:58:13 +0700

    add showModalReducer to store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers } from "redux";
 import cartReducer from "./features/cart/cartSlice";
 import { composeWithDevTools } from "@redux-devtools/extension";
+import showModalReducer from "./features/showModal/showModalSice";
 
 const rootReducer = combineReducers({
   cart: cartReducer,
+  showModal: showModalReducer,
 });
 
 const store = createStore(rootReducer, composeWithDevTools());
